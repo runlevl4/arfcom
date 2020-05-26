5459e762e153b6c9aa5efec15f10c7eddf2912d4
runlevl4 <root@runlevl4.com>
2020-05-25 21:49:56 -0400

    Refactored to add Chili handler

--- api/internal/handler/routes.go
+++ api/internal/handler/routes.go
@@ -1,10 +1,27 @@
 package handler
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 // Chili tries to answer the age-old question on Arfcom...beans or no beans.
 func Chili(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(100)
+	var s string
 
+	w.WriteHeader(http.StatusOK)
+
+	if n%2 == 0 {
+		s = "beans"
+	} else {
+		s = "no beans"
+	}
+	t := `{"answer": "%s"}`
+
+	fmt.Fprintf(w, t, s)
+	fmt.Fprintf(w, "\n")
 }
