3dd84fdd3e90eb68f6431ad7c1e92d28ee753006
runlevl4 <root@runlevl4.com>
2020-05-26 20:28:52 -0400

    Added constants for content-type

--- api/internal/handler/routes.go
+++ api/internal/handler/routes.go
@@ -7,12 +7,16 @@ import (
 	"time"
 )
 
+const content = "Content-Type"
+const contentJSON = "application/json"
+
 // Chili tries to answer the age-old question on Arfcom...beans or no beans.
 func Chili(w http.ResponseWriter, r *http.Request) {
 	rand.Seed(time.Now().UnixNano())
 	n := rand.Intn(100)
 	var s string
 
+	w.Header().Set(content, contentJSON)
 	w.WriteHeader(http.StatusOK)
 
 	if n%2 == 0 {
@@ -23,5 +27,23 @@ func Chili(w http.ResponseWriter, r *http.Request) {
 	t := `{"answer": "%s"}`
 
 	fmt.Fprintf(w, t, s)
+	fmt.Fprintf(w, "\n")
+}
+
+// Fu speaks to our infamous alum.
+func Fu(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	t := `{"message": "FU AROCK!"}`
+	fmt.Fprintf(w, t)
+	fmt.Fprintf(w, "\n")
+}
+
+// Caliber tries to steer the caller to the right pistol choice.
+func Caliber(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusInternalServerError)
+	t := `{"error": "divide by zero"}`
+	fmt.Fprintf(w, t)
 	fmt.Fprintf(w, "\n")
 }
