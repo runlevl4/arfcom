60159667bebdcec6b6c9c8672bf8293a6398ac37
runlevl4 <root@runlevl4.com>
2020-05-26 20:32:42 -0400

    Refactored handlers into separate package

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/runlevl4/arfcom/api/internal/handler"
 )
 
 func main() {
@@ -22,9 +21,9 @@ func run() error {
 	var log = log.New(os.Stdout, "arfcom|", log.LstdFlags|log.Lmicroseconds)
 
 	r := mux.NewRouter()
-	r.HandleFunc("/chili", loggingMiddleware(Chili)).Methods("GET")
-	r.HandleFunc("/fu", loggingMiddleware(Fu)).Methods("GET")
-	r.HandleFunc("/caliber/9mmOr45", loggingMiddleware(Caliber)).Methods("GET")
+	r.HandleFunc("/chili", loggingMiddleware(handler.Chili)).Methods("GET")
+	r.HandleFunc("/fu", loggingMiddleware(handler.Fu)).Methods("GET")
+	r.HandleFunc("/caliber/9mmOr45", loggingMiddleware(handler.Caliber)).Methods("GET")
 
 	log.Printf("%s : Starting server on port 8000", time.Now().Format(time.RFC3339))
 	log.Fatal(http.ListenAndServe(":8000", r))
@@ -41,41 +40,4 @@ func loggingMiddleware(handler HandleFunc) HandleFunc {
 		handler(w, r)
 	}
 	return fn
-}
-
-// Chili tries to answer the age-old question on Arfcom...beans or no beans.
-func Chili(w http.ResponseWriter, r *http.Request) {
-	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(100)
-	var s string
-
-	w.WriteHeader(http.StatusOK)
-
-	if n%2 == 0 {
-		s = "beans"
-	} else {
-		s = "no beans"
-	}
-	t := `{"answer": "%s"}`
-
-	fmt.Fprintf(w, t, s)
-	fmt.Fprintf(w, "\n")
-}
-
-// Fu speaks to our infamous alum.
-func Fu(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	t := `{"message": "FU AROCK!"}`
-	fmt.Fprintf(w, t)
-	fmt.Fprintf(w, "\n")
-}
-
-// Caliber tries to steer the caller to the right pistol choice.
-func Caliber(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusInternalServerError)
-	t := `{"error": "divide by zero"}`
-	fmt.Fprintf(w, t)	
-	fmt.Fprintf(w, "\n")
 }
